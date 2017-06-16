d48848c46b5883d493457c96fb73caedd518e67b
Mayukh Chakraborty <mayukhchakraborty40@gmail.com>
2017-06-17 02:07:09 +0530

    upd

---
+++ app/src/main/java/in/jewelchat/jewelchat/screens/ActivityJewelFactories.java
@@ -0,0 +1,29 @@
+package in.jewelchat.jewelchat.screens;
+
+import android.os.Bundle;
+import android.view.View;
+
+import in.jewelchat.jewelchat.BaseNetworkActivity;
+import in.jewelchat.jewelchat.R;
+
+/**
+ * Created by mayukhchakraborty on 16/06/17.
+ */
+
+public class ActivityJewelFactories extends BaseNetworkActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_factories);
+
+
+
+	}
+
+
+	@Override
+	public void onClick(View view) {
+
+	}
+}
